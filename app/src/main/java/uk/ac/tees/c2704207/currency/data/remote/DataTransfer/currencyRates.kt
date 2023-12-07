bf49152212cebff421cb6a537d96a1d77f8a138a
mahassan110 <C2704207@live.yees.ac.uk>
2023-12-07 23:01:15 +0000

    project

---
+++ app/src/main/java/uk/ac/tees/c2704207/currency/data/remote/DataTransfer/currencyRates.kt
@@ -0,0 +1,5 @@
+package uk.ac.tees.c2704207.currency.data.remote.DataTransfer
+
+data class currencyRates(
+    val data: CurrencyDataTransfer
+)
---
+++ app/src/main/java/uk/ac/tees/c2704207/currency/data/remote/currencyAPI.kt
@@ -0,0 +1,21 @@
+package uk.ac.tees.c2704207.currency.data.remote
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+import uk.ac.tees.c2704207.currency.data.remote.DataTransfer.CurrencyDataTransfer
+
+interface currentAPI{
+
+    @GET("v1/latest")
+        suspend fun getLatestRates(
+
+            @Query("apikey")  apikey : String = API_KEY
+
+        ) : CurrencyDataTransfer
+        companion object{
+
+            const val API_KEY = "fca_live_du3jMyPEMDmzIROBMmv9SG33heIQY5uJzaTfuRSk"
+            const val BASE_API = "https://api.freecurrencyapi.com/"
+
+        }
+}
